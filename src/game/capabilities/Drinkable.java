48cecf2a5c8046a9fd32663e0e3c3366783b62dd
fsun0022 <fsun0022@student.monash.edu>
2025-09-11 18:58:01 +1000

    Create a capabilities package along with its contents: - Add Drinkable interface to capabilities package for a contract of the implementation for the Drink Action.
    - Add Sleepable interface to capabilities package for a contract of the implementation for the Sleep Action.
    
    This commit is used to allow loose coupling between the object and its action implementation.

---
+++ src/game/capabilities/Drinkable.java
@@ -0,0 +1,15 @@
+package game.capabilities;
+
+import edu.monash.fit2099.engine.actors.Actor;
+
+public interface Drinkable {
+  /**
+   * The drinkBy methods, which will provide the implementation
+   * for the Drink Action
+   *
+   * @param actor   is the actor object
+   * @return a string that inform what happened.
+   */
+  public String drinkBy(Actor actor);
+}
+
