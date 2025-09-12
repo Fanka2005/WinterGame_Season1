63befbb863f87395740672d4cde04866758f34eb
fsun0022 <fsun0022@student.monash.edu>
2025-09-13 02:35:26 +1000

    Revised Earth Class :
    - Add Bedroll and Bottle (with capacity of 5) item to the player inventory
    
    - Add PlayerAlive status to the player.

--- src/game/Earth.java
+++ src/game/Earth.java
@@ -1,11 +1,14 @@
 package game;
 
+import edu.monash.fit2099.engine.capabilities.Status;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.DefaultGroundCreator;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.World;
-
 import game.actors.Player;
+import game.items.Bedroll;
+import game.items.Bottle;
+import game.statuses.PlayerAlive;
 import java.util.Arrays;
 import java.util.List;
 
@@ -36,5 +39,11 @@ public class Earth extends World {
 
         Player player = new Player("Explorer", 'ඞ', 100);
         this.addPlayer(player, gameMap.at(1, 1));
+
+        player.addItemToInventory(new Bedroll());
+        player.addItemToInventory(new Bottle(5));
+
+        Status alive = new PlayerAlive(player);
+        player.addStatus(alive);
     }
 }
