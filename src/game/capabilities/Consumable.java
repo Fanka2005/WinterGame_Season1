287ae799b482a083f8af14fb607cfc9a4b05a0f4
fsun0022 <fsun0022@student.monash.edu>
2025-09-15 00:55:53 +1000

    Add Consumable interface into capabilities package:
    - Interface representing and is a signature for Consumable object

---
+++ src/game/capabilities/Consumable.java
@@ -0,0 +1,19 @@
+package game.capabilities;
+
+import edu.monash.fit2099.engine.actors.Actor;
+
+/**
+ * Interface representing and is a signature for Consumable object
+ *
+ * @author Fauzanda Lathifanka Sunarko
+ */
+public interface Consumable {
+
+  /**
+   * The consumedBy methods, which will provide the implementation for the Consumed Action
+   *
+   * @param actor is the actor object
+   * @return a string that inform what happened.
+   */
+  String consumedBy(Actor actor);
+}
